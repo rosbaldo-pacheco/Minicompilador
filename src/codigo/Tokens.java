fbe19debf5f7a9fcf5e54be851cfb2efb5116f08
Osbaldopacheco <you@example.com>
2021-05-27 22:00:49 -0600

    Parte2 A.Lexico y Sintactico

---
+++ src/codigo/Tokens.java
@@ -0,0 +1,43 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package codigo;
+
+/**
+ *
+ * @Osbal Pacheco
+ */
+public enum Tokens {
+    Linea,
+    Comillas,
+    T_dato,
+    Cadena,
+    If,
+    Else,
+    Do,
+    While,
+    For,
+    Igual,
+    Suma,
+    Resta,
+    Multiplicacion,
+    Division,
+    Op_logico,
+    Op_incremento,
+    Op_relacional,
+    Op_atribucion,
+    Op_booleano,
+    Parentesis_a,
+    Parentesis_c,
+    Llave_a,
+    Llave_c,
+    Corchete_a,
+    Corchete_c,
+    Main,
+    P_coma,
+    palabra_reservada,
+    Numero,
+    ERROR
+}
